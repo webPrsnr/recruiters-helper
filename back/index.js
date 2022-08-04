d880590ad07688a6f97dca49dc2161457ef39e39
William <webuntermench@protonmail.com>
2022-08-04 15:08:05 +0300

    update index.js

---
+++ back/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const sequelize = require("./database/database");
+const cors = require("cors");
+const resumRouter = require("./routes/v1/resumeRoutes");
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/v1", resumRouter);
+
+const start = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log("DB is running");
+    app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
+  } catch {
+    console.log(e);
+  }
+};
+
+start();
